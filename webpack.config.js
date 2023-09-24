ff0f21ab34a766021fb73cafb59955f933d36a55
rishabh <rishabhsingh2305@gmail.com>
2023-09-25 00:20:54 +0530

    changes the output folder

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	},
 	output: {
 		filename: "main.js",
-		path: path.resolve(__dirname, "public"),
+		path: path.resolve(__dirname, "dist"),
 	},
 	mode: process.env.NODE_ENV === "production" ? "production" : "development",
 };
